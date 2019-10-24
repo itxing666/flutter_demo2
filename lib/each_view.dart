01f3d3a459ad1be77dd200c4d52962d0152a65b5
itxing <itxing666@163.com>
2019-10-24 08:59:32 +0800

    修改

---
+++ lib/each_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EachView extends StatefulWidget {
+  @override
+  _EachViewState createState() => _EachViewState();
+}
+
+class _EachViewState extends State<EachView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
